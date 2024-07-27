8a358302ed45dc8ab81ebfb6261382114e27f655
cangzhishang <cangzhishang@gmail.com>
2024-07-27 15:37:32 -0500

    setup database, rewrite user model

--- OASystemBackend/settings.py
+++ OASystemBackend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,13 +35,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # install rest_framework
+    'rest_framework',
+    'corsheaders',
+    'apps.oaauth'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # should before CommonMiddleware
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #  close CSRF protection
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -70,17 +75,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'OASystemBackend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oa',
+        'USER': 'root',
+        'PASSWORD': 'fengxiao',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -112,7 +118,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -122,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# cover django user model
+AUTH_USER_MODEL = 'oaauth.OAUser'
---
+++ apps/oaauth/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import User, AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+
+# Create your models here.
+
+class UserStatusChoices(models.IntegerChoices):
+    ACTIVATED = 1
+    INACTIVATED = 2
+    LOCKED = 3
+class OAUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, realname, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not realname:
+            raise ValueError("The realname must be set")
+        email = self.normalize_email(email)
+        user = self.model(realname=realname, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, realname, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(realname, email, password, **extra_fields)
+
+    def create_superuser(self, realname, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault('status',UserStatusChoices.ACTIVATED)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(realname, email, password, **extra_fields)
+
+
+
+
+class OAUser(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username and password are required. Other fields are optional.
+    """
+
+    realname = models.CharField(
+        max_length=150,
+        unique=False
+    )
+    email = models.EmailField(unique=True, blank=False)
+    telephone = models.CharField(max_length=20, blank=True)
+    is_staff = models.BooleanField(default=True)
+    status = models.IntegerField(choices=UserStatusChoices, default=UserStatusChoices.INACTIVATED)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = OAUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["realname", 'password']
+
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        return self.realname
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.realname
+
