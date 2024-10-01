a7f7eae2bef63c4de9a305016561cd8e47f984bb
Xiao <cangqiongyishao@gmail.com>
2024-09-30 19:55:27 -0500

    absent views and serializers part 4

--- apps/absent/views.py
+++ apps/absent/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
+
+from rest_framework.response import Response
 from .models import Absent, AbsentType, AbsentStatusChoices
-from .serializers import AbsentSerializer
+from .serializers import AbsentSerializer, AbsentTypeSerializer
 
 
 # Create your views here.
@@ -16,3 +18,15 @@ class AbsentViewSet(mixins.CreateModelMixin,
     def update(self, request, *args, **kwargs):
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        who=request.query_params.get('who')
+        if who and who =='sub':
+            result=queryset.filter(responder=request.user)
+        else:
+            result=queryset.filter(requester=request.user)
+
+        serializer=self.serializer_class(result,many=True)
+        return Response(data=serializer.data)
+
